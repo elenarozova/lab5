af4715c73f70fabe6cf86171215dcca994b6e359
elena <elenarozova13@gmail.com>
2026-03-12 00:36:17 +0300

    Исправила remove_greater

--- src/commands/RemoveGreater.java
+++ src/commands/RemoveGreater.java
@@ -4,6 +4,9 @@ import data.LabWork;
 import interfaces.Comands;
 import program.Program;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Команда для удаления всех элементов, которые больше заданного.
  * Пользователь вводит элемент, затем все элементы коллекции,
@@ -22,7 +25,8 @@ public class RemoveGreater implements Comands {
         if (args.length==0) {
             Program.inout.write("Введите элемент, с которым будут сравниваться другие");
             LabWork lab = new LabWork();
-            for (int i : Program.colman.getLabWork().keySet()) {
+            List<Integer> keyS = new ArrayList<>(Program.colman.getLabWork().keySet());
+            for (int i : keyS) {
                 if (lab.compareTo(Program.colman.getLabWork().get(i)) < 0) {
                     Program.colman.getLabWork().remove(i);
                 }
